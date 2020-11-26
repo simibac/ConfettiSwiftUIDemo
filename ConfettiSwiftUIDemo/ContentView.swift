df532b873987d7f2304d22b211975c3db884c148
Simon <simibac93@gmail.com>
2020-11-26 17:37:52 +0100

    initial commit

--- ConfettiSwiftUIDemo/ContentView.swift
+++ ConfettiSwiftUIDemo/ContentView.swift
@@ -6,13 +6,77 @@
 //
 
 import SwiftUI
+import ConfettiSwiftUI
 
 struct ContentView: View {
+    @State var counter1:Int = 0
+    @State var counter2:Int = 0
+    @State var counter3:Int = 0
+    @State var counter4:Int = 0
+    @State var counter5:Int = 0
+    @State var counter6:Int = 0
+
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            ZStack{
+                Text("🎉").font(.system(size: 50)).onTapGesture(){counter1 += 1}
+                ConfettiCannon(counter: $counter1)
+            }
+            .tabItem {
+                Image(systemName: "sparkles")
+                Text("Emoji")
+            }
+            
+            ZStack{
+                Text("🎊").font(.system(size: 50)).onTapGesture(){counter2 += 1}
+                ConfettiCannon(counter: $counter2, colors: [.red, .black], confettiSize: 20)
+            }
+            .tabItem {
+                Image(systemName: "eyedropper")
+                Text("Colors")
+            }
+            
+            ZStack{
+                Text("🔁").font(.system(size: 50)).onTapGesture(){counter3 += 1}
+                ConfettiCannon(counter: $counter3, repetitions: 3, repetitionInterval: 0.7)
+            }
+            .tabItem {
+                Image(systemName: "repeat")
+                Text("Repeat")
+            }
+            
+            ZStack{
+                Text("🧨").font(.system(size: 50)).onTapGesture(){counter4 += 1}
+                ConfettiCannon(counter: $counter4, num: 50, openingAngle: Angle(degrees: 0), closingAngle: Angle(degrees: 360), radius: 200)
+            }
+            .tabItem {
+                Image(systemName: "flame")
+                Text("Fireworks")
+            }
+            
+            ZStack{
+                Text("❤️").font(.system(size: 50)).onTapGesture(){counter5 += 1}
+                ConfettiCannon(counter: $counter5, emojis: ["❤️", "💙", "💚", "🧡"])
+            }
+            .tabItem {
+                Image(systemName: "suit.heart")
+                Text("Emoji")
+            }
+            
+            ZStack{
+                Text("💩").font(.system(size: 50)).onTapGesture(){counter6 += 1}
+                ConfettiCannon(counter: $counter6, num:1, emojis: ["💩"], confettiSize: 20, repetitions: 100, repetitionInterval: 0.1)
+            }
+            .tabItem {
+                Image(systemName: "umbrella")
+                Text("Constant")
+            }
+            
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
