3e4861fa6a39919e9f1bb7b8c5791d660124daad
Simon <simibac93@gmail.com>
2020-11-26 16:12:05 +0100

    Initial Commit

---
+++ ConfettiSwiftUIDemo/ConfettiSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfettiSwiftUIDemoApp.swift
+//  ConfettiSwiftUIDemo
+//
+//  Created by Simon Bachmann on 26.11.20.
+//
+
+import SwiftUI
+
+@main
+struct ConfettiSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConfettiSwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConfettiSwiftUIDemo
+//
+//  Created by Simon Bachmann on 26.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
